1bd0e5ac1f0085479ea897d884f1ecdb5cda3014
Luke Chi <luke.chi@gmail.com>
2020-12-19 14:29:18 +0800

    revise

--- src/main/java/com/nv/tech/ideavim/DemoTest.java
+++ src/main/java/com/nv/tech/ideavim/DemoTest.java
@@ -9,21 +9,23 @@ import org.junit.Test;
  */
 public class DemoTest {
 
-	/*
+	/**
 	jtest
 	Tennis.new
 	zl.var
+	zn
 	o
 	tennis.score()
 	alt enter
-	v w vw
-	xa
+	w xa
+	ctrl shift enter
 	.ae
+	ctrl shift r
 	 */
-
 	@Test
 	public void testLoveAll() {
 		Tennis tennis = new Tennis();
 		assertEquals("love all", tennis.score());
 	}
+
 }
